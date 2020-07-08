e57dcb6a9d0e70f5b2de8e24bfb8908c3a815a9a
prudhvi-v <prudhvi.veebhuthi@HMCTS.NET>
2020-07-08 01:49:39 +0100

    Updating config

--- default.conf.js
+++ default.conf.js
@@ -16,6 +16,7 @@ exports.config = {
       browser: process.env.BROWSER || 'chrome',
       url: process.env.LEGAL_APP_URL || 'https://localhost:3000',
       waitForTimeout: 15000,
+      restart: false,
       desiredCapabilities: {
         proxy: new ProxySettings(),
         "acceptInsecureCerts": true
